3768a3d6031b39238c5867d1cc86d79a63c53bba
blu3tan <142602465+blu3tan@users.noreply.github.com>
2024-05-13 20:58:37 +0200

    Fix: refactor class constructor

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,17 +1,18 @@
-function generateBoard() {
-  const board = [];
-  for (let i = 0; i <= 9; i += 1) {
-    const column = [];
-    for (let j = 0; j <= 9; j += 1) {
-      column.push([]);
-    }
-    board.push(column);
-  }
-  return board;
-}
-
 export default class GameBoard {
   constructor() {
-    this.gameBoard = generateBoard();
+    this.gameBoard = [];
+    this.generateBoard();
+  }
+
+  generateBoard() {
+    const board = [];
+    for (let i = 0; i <= 9; i += 1) {
+      const column = [];
+      for (let j = 0; j <= 9; j += 1) {
+        column.push([]);
+      }
+      board.push(column);
+    }
+    this.gameBoard = board;
   }
 }
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -3,11 +3,11 @@ import GameBoard from "../gameBoard";
 describe("Game board class", () => {
   const testBoard = new GameBoard();
 
-  it("Should have a board item", () => {
+  it("Should have a gameBoard item", () => {
     expect(testBoard).toHaveProperty("gameBoard");
   });
 
-  it("Should have x/y coordinates", () => {
+  it("GameBoard should be 10 by 10", () => {
     const resultBoard = testBoard.gameBoard;
     expect(resultBoard[0][0]).toStrictEqual([]);
     expect(resultBoard[9][9]).toStrictEqual([]);
